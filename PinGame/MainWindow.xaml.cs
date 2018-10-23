1f44e055b8d56f5e4887ee72ba2204550de0e13f
danielegtved <43374944+danielegtved@users.noreply.github.com>
2018-10-23 11:19:25 +0200

    Fixed rows and text block

--- PinGame/MainWindow.xaml.cs
+++ PinGame/MainWindow.xaml.cs
@@ -21,12 +21,16 @@ namespace PinGame
     {
         private int row1 = 1;
         private int row2 = 3;
-
+        private int row3 = 5;
+        private int row4 = 7;
     
         public MainWindow()
         {
             InitializeComponent();
             row1Textblock.Text = "" + row1;
+            row2Textblock.Text = "" + row2;
+            row3Textblock.Text = "" + row3;
+            row4Textblock.Text = "" + row4;
         }
 
         private void Row1_Click(object sender, RoutedEventArgs e)
@@ -37,15 +41,23 @@ namespace PinGame
 
         private void Row2_Click(object sender, RoutedEventArgs e)
         {
-
+            row2--;
+            row2Textblock.Text = "" + row2;
         }
 
         private void Row3_Click(object sender, RoutedEventArgs e)
         {
-
+            row3--;
+            row3Textblock.Text = "" + row3;
         }
 
         private void Row4_Click(object sender, RoutedEventArgs e)
+        {
+            row4--;
+            row4Textblock.Text = "" + row4;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
