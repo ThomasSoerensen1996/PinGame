2858f88133b7cf99fbea72d007814e4ad1b8d632
danielegtved <43374944+danielegtved@users.noreply.github.com>
2018-10-23 13:38:47 +0200

    Made game

--- PinGame/MainWindow.xaml.cs
+++ PinGame/MainWindow.xaml.cs
@@ -23,8 +23,8 @@ namespace PinGame
         private int row2 = 3;
         private int row3 = 5;
         private int row4 = 7;
-        private string player1;
-        private string player2;
+        private bool isPlayer1 = false;
+        private bool gameover = false;
     
         public MainWindow()
         {
@@ -37,29 +37,143 @@ namespace PinGame
 
         private void Row1_Click(object sender, RoutedEventArgs e)
         {
-            row1--;
-            row1Textblock.Text = ""+row1;
+            Row2.IsEnabled = false;
+            Row3.IsEnabled = false;
+            Row4.IsEnabled = false;
+            if (row1 == 1)
+            {
+                row1--;
+                row1Textblock.Text = "" + row1;
+                Row1.IsEnabled = false;
+            }
+            Gameover();
+            GameEnd();
+
         }
 
         private void Row2_Click(object sender, RoutedEventArgs e)
         {
-            row2--;
-            row2Textblock.Text = "" + row2;
+            Row1.IsEnabled = false;
+            Row3.IsEnabled = false;
+            Row4.IsEnabled = false;
+            if (row2 > 1)
+            {
+                row2--;
+                row2Textblock.Text = "" + row2;
+            }
+            else if (row2 == 1)
+            {
+                row2--;
+                row2Textblock.Text = "" + row2;
+                Row2.IsEnabled = false;
+            }
+            Gameover();
+            GameEnd();
         }
 
         private void Row3_Click(object sender, RoutedEventArgs e)
         {
-            row3--;
-            row3Textblock.Text = "" + row3;
+            Row1.IsEnabled = false;
+            Row2.IsEnabled = false;
+            Row4.IsEnabled = false;
+            if (row3 > 1)
+            {
+                row3--;
+                row3Textblock.Text = "" + row3;
+            }
+            else if (row3 == 1)
+            {
+                row3--;
+                row3Textblock.Text = "" + row3;
+                Row3.IsEnabled = false;
+            }
+            Gameover();
+            GameEnd();
         }
 
         private void Row4_Click(object sender, RoutedEventArgs e)
         {
-            row4--;
-            row4Textblock.Text = "" + row4;
+            Row1.IsEnabled = false;
+            Row2.IsEnabled = false;
+            Row3.IsEnabled = false;
+            if (row4 > 1)
+            {
+                row4--;
+                row4Textblock.Text = "" + row4;
+            }
+            else if (row4 == 1)
+            {
+                row4--;
+                row4Textblock.Text = "" + row4;
+                Row4.IsEnabled = false;
+            }
+          
+            Gameover();
+            GameEnd();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlayer1)
+            {
+                PlayerBox.Text = "Player 1";
+                isPlayer1 = false;
+            }
+            else
+            {
+                PlayerBox.Text = "Player 2";
+                isPlayer1 = true;
+            }
+
+            if (row1 > 0)
+            {
+                Row1.IsEnabled = true;
+            }
+
+            if (row2 > 0)
+            {
+                Row2.IsEnabled = true;
+            }
+            if (row3 > 0)
+            {
+                Row3.IsEnabled = true;
+            }
+            if (row4 > 0)
+            {
+                Row4.IsEnabled = true;
+            }
+        }
+
+        public void Gameover()
+        {
+            if (row1 + row2 + row3 + row4 == 1)
+            {
+                gameover = true;
+            }
+        }
+
+        public void GameEnd()
+        {
+            if (gameover == true)
+            {
+                Row1.IsEnabled = false;
+                Row2.IsEnabled = false;
+                Row3.IsEnabled = false;
+                Row4.IsEnabled = false;
+                changePlayer.IsEnabled = false;
+                if (isPlayer1)
+                {
+                    gameEndText.Text = "Game has ended Player 2 wins";
+                }
+                else 
+                {
+                    gameEndText.Text = "Game has ended Player 1 wins";
+                }
+
+            }
+        }
+
+        public void freeze()
         {
 
         }
